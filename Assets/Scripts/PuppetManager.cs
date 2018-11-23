00d35b1f165e4b450e62b9015ccc432da0f39c12
Mikayla <mikaylajones@u.boisestate.edu>
2018-11-23 15:44:20 -0700

    AI work
    Spirits now take vegetables and destroy them when their destination is reached

---
+++ Assets/Scripts/PuppetManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PuppetManager : NetworkBehaviour {
+
+    private System.Action _followingAction;
+
+    public Transform PuppetEmpty;
+    public Transform PuppetBody;
+    public Transform PuppetHead;
+    public Transform L_PuppetHand;
+    public Transform R_PuppetHand;
+
+    public Transform BodyTarget;
+    public Transform HeadTarget;
+    public Transform L_HandTarget;
+    public Transform R_HandTarget;
+
+    private void Awake()
+    {
+        _followingAction = FollowingInactive;
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        //Find Client Side OVR Targets
+        BodyTarget = GameObject.Find("OVRPlayerController").transform;
+        HeadTarget = GameObject.Find("CenterEyeAnchor").transform;
+        L_HandTarget = GameObject.Find("LeftHandAnchor").transform;
+        R_HandTarget = GameObject.Find("RightHandAnchor").transform;
+
+        this._followingAction = FollowingActive;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        _followingAction.Invoke();
+
+	}
+
+    private void FollowingInactive()
+    {
+        return;
+    }
+
+    private void FollowingActive()
+    {
+        //Update Positions
+        PuppetEmpty.position = BodyTarget.position;
+
+        PuppetBody.position = BodyTarget.position;
+
+        PuppetHead.position = HeadTarget.position;
+        PuppetHead.rotation = HeadTarget.rotation;
+
+        L_PuppetHand.position = L_HandTarget.position;
+        L_PuppetHand.rotation = L_HandTarget.rotation;
+
+        R_PuppetHand.position = R_HandTarget.position;
+        R_PuppetHand.rotation = R_HandTarget.rotation;
+
+    }
+}
---
+++ Assets/Scripts/SpiritSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritSpawner : MonoBehaviour {
+
+    GameObject[] spawnPoints;
+    GameObject newSpawn;
+    int index;
+
+    public int availableSpirits = 15;
+    public int maxSpirits = 5;
+    int currentSpirits = 0;
+    int lifetimeSpirits;
+
+    List<GameObject> existingSpirits;
+
+    public GameObject spiritToSpawn;
+
+
+    public float targetTime = 20.0f;
+
+
+    // Use this for initialization
+    void Start () {
+        spawnPoints = GameObject.FindGameObjectsWithTag("Shrine");
+        index = Random.Range(0, spawnPoints.Length);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 0.0f)
+        {
+            SpawnSpirit();
+        }
+    }
+
+    void SpawnSpirit()
+    {
+        //if(existingSpirits.Count <= maxSpirits && lifetimeSpirits <= availableSpirits)
+        //{
+            index = Random.Range(0, spawnPoints.Length);
+            newSpawn = spawnPoints[index];
+            spiritToSpawn = Instantiate(spiritToSpawn);
+
+            spiritToSpawn.transform.position = newSpawn.transform.position;
+            
+            lifetimeSpirits++;
+            targetTime = 20.0f;
+        //}
+
+    }
+}
